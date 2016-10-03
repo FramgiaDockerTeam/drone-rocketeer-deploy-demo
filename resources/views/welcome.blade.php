e680161ebb418356ea1df58575b1a067784b90e1
TungPro <mr.nttung@gmail.com>
2016-10-03 13:21:26 +0700

    Update to version 13

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-10 col-md-offset-1">
             <h1 class="text-primary text-center">You are in <strong>{{ app()->environment() }} </strong> environment!</h1>
             <h2 class="text-success text-center">Server Local IP: <strong>{{ isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : 'local' }}</strong></h2>
-            <h3 class="text-danger text-center">Version: <strong>12</strong></h3>
+            <h3 class="text-danger text-center">Version: <strong>13</strong></h3>
             <br>
             <div class="panel panel-default">
                 <div class="panel-heading">Welcome to a Demo Site</div>
